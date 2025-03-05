728d2f2bb1f1f462c2e41c8020599ad6eca7bf95
Fernando Ibarra <luisfernandoibarrao@gmail.com>
2025-03-05 19:32:34 -0400

    Pruebas Unitarias PHP

---
+++ tests/Facturacion/operacion_Test.php
@@ -0,0 +1,20 @@
+<?php
+declare (strict_types=1);
+use PHPUnit\Framework\TestCase;
+include "./src/operacion.php";
+class operacion_Test extends TestCase{
+    public function testFactorial():void{
+        $objOpr= new operacion();
+        $nFct= $objOpr->suma(20,30);
+        $this->assertIsNumeric($nFct);
+        $this->assertEquals($nFct,50);
+    }
+    public function testuno():void
+    {
+        $this->assertTrue(false);
+    }
+    public function testdos():void{
+        $this->assertTrue(true);
+    }
+
+}
